dcb37aef94273ca0f92eaa25ef0e590012909324
Prendicito <jonathanttvbtw@gmail.com>
2025-10-05 21:45:23 -0600

    Create Clase 04 - 03.py

---
+++ Semana 04/Clase 04 - 03.py
@@ -0,0 +1,22 @@
+"""Itemingresar = input("ingrese el item: ")
+print(f"El item ingresado es {Itemingresar}")
+#alvin mi goat
+
+s1 = ""
+s2 = " "
+s3 = "A B C"
+print(len(s1))
+print(len(s2))
+print(len(s3))"""
+
+my_string = "ABCDEFGHCCCI"
+"""Letra = int(input("Indique el indice de la letra a mostrar(del 0 al 4): "))
+print(my_string[Letra])"""
+print(len(my_string))
+print(my_string[1:5:3])
+print("H" in my_string)
+print("A"* 8 , "SOLTAMEEEEEE")
+
+print(min(my_string))
+print(max(my_string))
+print(my_string.count("C"))
