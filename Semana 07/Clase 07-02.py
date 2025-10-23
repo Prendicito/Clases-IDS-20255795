cbad9380fc192fe7b855f6d744664c883b8120c6
Prendicito <jonathanttvbtw@gmail.com>
2025-10-23 16:48:53 -0600

    clase de semana 7

---
+++ Semana 07/Clase 07-02.py
@@ -0,0 +1,33 @@
+"""nombres = ["Ana", "Antonio", "Ana", "Jose","Maria"]
+repetidos_a = 0
+print(nombres.count("Maria"))
+repetidos_a = repetidos_a + nombres[0].lower().count("a")
+print(repetidos_a)
+
+print(nombres.index("Ana",1))
+print(nombres.index("Ana", nombres.index("Ana")+1))
+
+nombres.append("Abby")
+nombres.insert(2,"Jorge")
+nombres.insert(int(input("Posicion:")),input("Nombre nuevo: "))
+print(nombres)
+
+
+nombres.remove("Jose")
+nombre_borrado = nombres.pop(3)
+print(f"Nombre borrado: {nombre_borrado}")
+print(nombres)
+nombres.reverse()
+print(nombres)"""
+#segunda parte, es que ya se me hizo muy largo aca el archivo
+#aca vemos el if else
+
+numero = 6
+captura = int(input("Adivina el numero: "))
+
+if captura == numero:
+    print("Lo lograsteeeee soltame gorda nooo como va a traer a la femigorda aaay dios mio clarence")
+else:
+    print("Manco manco")
+
+
